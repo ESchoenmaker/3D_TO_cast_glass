a8edd509e728749672c76c17556b3a3bd85909fe
ESchoenmaker <148453923+ESchoenmaker@users.noreply.github.com>
2023-10-19 14:06:00 +0200

    Delete f_read_data_gh.m

--- f_read_data_gh.m
+++
@@ -1,7 +0,0 @@
-function [d_FE, Length, Width, Height] = f_read_data_gh(name_excel, sheet_name)
-
-data = readmatrix(name_excel, 'Sheet',sheet_name, 'Range', 'A2:D2');
-d_FE = data(1);
-Length = data(2);
-Width = data(3);
-Height = data(4);
